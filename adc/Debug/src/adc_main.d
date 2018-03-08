eb12b2aa4f52cda058beaa8added154cb5514432
kaylakloster11 <kakl3031@colorado.edu>
2018-03-07 20:05:03 -0700

    Second Lab

---
+++ adc/Debug/src/adc_main.d
@@ -0,0 +1,23 @@
+src/adc_main.o src/adc_main.d: ../src/adc_main.c \
+ ../config/driver_config.h ../cmsis/LPC11xx.h ../cmsis/core_cm0.h \
+ ../config/system_LPC11xx.h ../config/target_config.h ../driver/adc.h \
+ ../driver/gpio.h ../driver/debug_printf.h \
+ C:\Users\derek\Documents\LPCXpresso_8.2.2_650\workspace\lib_small_printf_m0\inc/small_printf.h
+
+../config/driver_config.h:
+
+../cmsis/LPC11xx.h:
+
+../cmsis/core_cm0.h:
+
+../config/system_LPC11xx.h:
+
+../config/target_config.h:
+
+../driver/adc.h:
+
+../driver/gpio.h:
+
+../driver/debug_printf.h:
+
+C:\Users\derek\Documents\LPCXpresso_8.2.2_650\workspace\lib_small_printf_m0\inc/small_printf.h:
